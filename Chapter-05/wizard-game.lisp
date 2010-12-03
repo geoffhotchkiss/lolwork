8587521b7183a6d2d6ab59c06f0eefead3f6edb1
= <geoff@csh.rit.edu>
2010-12-03 16:35:22 -0500

    Expanded tabs to see if it helps any

--- Chapter-05/wizard-game.lisp
+++ Chapter-05/wizard-game.lisp
@@ -2,11 +2,11 @@
 ; possible rooms as well as descriptions of the rooms. What's interesting is
 ; that the descriptions are not strings, rather they are symbols/data. 
 (defparameter *nodes* '((living-room (you are in the living room. 
-										  a wizard is snoring loudly on the couch))
-						(garden (you are in a beautiful garden. there is a well in front
-									 of you.))
-						(attic (you are in the attic. there is a giant welding torch in
-									the corner.))))
+                                          a wizard is snoring loudly on the couch))
+                        (garden (you are in a beautiful garden. there is a well in front
+                                     of you.))
+                        (attic (you are in the attic. there is a giant welding torch in
+                                    the corner.))))
 
 ; Describes the location passed in. The node list needs to be given which
 ; conatins the information for the location. 
